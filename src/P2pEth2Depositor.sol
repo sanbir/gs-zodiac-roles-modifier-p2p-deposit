36dca2fdb94d80b0a60d457a11ed7aaef1ec1bec
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-08-30 14:51:05 +0300

    getScopeAllowFunctionCalldata

--- src/P2pEth2Depositor.sol
+++ src/P2pEth2Depositor.sol
@@ -4,5 +4,17 @@
 pragma solidity 0.8.10;
 
 interface P2pEth2Depositor {
+    struct FeeRecipient {
+        uint96 basisPoints;
+        address payable recipient;
+    }
 
+    function deposit(
+        bytes[] calldata _pubkeys,
+        bytes calldata _withdrawal_credentials,
+        bytes[] calldata _signatures,
+        bytes32[] calldata _deposit_data_roots,
+        FeeRecipient calldata _clientConfig,
+        FeeRecipient calldata _referrerConfig
+    ) external payable;
 }
--- test/Deposit.t.sol
+++ test/Deposit.t.sol
@@ -12,8 +12,10 @@ import "../src/GnosisSafe.sol";
 import "../src/Roles.sol";
 import "../src/TimelockController.sol";
 import "../src/MultiSendCallOnly.sol";
+import "../src/P2pEth2Depositor.sol";
 
 contract Deposit is Test {
+    uint16 constant ROLE = 1;
     MultiSendCallOnly constant multiSendCallOnly = MultiSendCallOnly(0x40A2aCCbd92BCA938b02010E17A5b8929b49130D);
 
 //    TimelockController constant ensTimelockController = TimelockController(0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7); // ensSafe owner
@@ -25,6 +27,7 @@ contract Deposit is Test {
     GnosisSafe constant ensSafe = GnosisSafe(0x14d91faAca6a7aa4C1ac371C15425eAc5A75dADB); // roles owner
     Roles constant roles = Roles(0xa8824FE7760a06E2587C86b33640C981b5E31e0D);
     GnosisSafe constant pilotSafe = GnosisSafe(0x32795D2374A047e1B8591463cDB8E5B34c6dd89D);
+    P2pEth2Depositor p2pEth2Depositor = P2pEth2Depositor(0x2E0743aAAB3118945564b715598B7DF10e083dC1);
 
     function setUp() public {
         // vm.createSelectFork("mainnet");
@@ -38,18 +41,44 @@ contract Deposit is Test {
         vm.startPrank(address(ensTimelockController));
 
         bytes memory assignRolesCalldata = getAssignRolesCalldata();
-
         console.logBytes(assignRolesCalldata);
+
+        bytes memory allowTargetCalldata = getAllowTargetCalldata();
+        console.logBytes(allowTargetCalldata);
+
+        bytes memory scopeAllowFunctionCalldata = getScopeAllowFunctionCalldata();
+        console.logBytes(scopeAllowFunctionCalldata);
     }
 
     function getAssignRolesCalldata() private pure returns(bytes memory assignRolesCalldata) {
         address module = address(pilotSafe);
         uint16[] memory _roles = new uint16[](1);
-        _roles[0] = 1;
+        _roles[0] = ROLE;
         bool[] memory memberOf = new bool[](1);
         memberOf[0] = true;
         assignRolesCalldata = abi.encodeWithSelector(
             Roles.assignRoles.selector, module, _roles, memberOf
+        );
+    }
+
+    function getAllowTargetCalldata() private view returns(bytes memory allowTargetCalldata) {
+        uint16 role = ROLE;
+        address targetAddress = address(p2pEth2Depositor);
+        Roles.ExecutionOptions options = Roles.ExecutionOptions.Send;
+
+        allowTargetCalldata = abi.encodeWithSelector(
+            Roles.allowTarget.selector, role, targetAddress, options
+        );
+    }
+
+    function getScopeAllowFunctionCalldata() private view returns(bytes memory scopeAllowFunctionCalldata) {
+        uint16 role = ROLE;
+        address targetAddress = address(p2pEth2Depositor);
+        bytes4 functionSig = P2pEth2Depositor.deposit.selector;
+        Roles.ExecutionOptions options = Roles.ExecutionOptions.None;
+
+        scopeAllowFunctionCalldata = abi.encodeWithSelector(
+            Roles.scopeAllowFunction.selector, role, targetAddress, functionSig, options
         );
     }
 }
